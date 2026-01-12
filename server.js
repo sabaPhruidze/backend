e1a903abee7c94b5000a7ce14b41c6366df30f2b
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 16:11:16 +0400

    now I tested /about page. where I used query ,which givers information in object , everything after ? sign written, for sorting for example only age 24, or only role admin so this will be used for that, as for post /put there we probably use body

--- server.js
+++ server.js
@@ -5,27 +5,38 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, name: "Kate", role: "Admin", age: 22 },
+  { id: 2, name: "Nino", role: "User", age: 22 },
+  { id: 3, name: "Anna", role: "Editor", age: 24 },
+];
+
 app.get("/", (req, res) => {
   res.send("Send only returns string");
 });
 app.get("/api/users/:id", (req, res) => {
   // let's change this and make dynamic
-  const users = [
-    { id: 1, name: "Kate", role: "Admin", age: 25 },
-    { id: 2, name: "Nino", role: "User", age: 22 },
-    { id: 3, name: "Anna", role: "Editor", age: 24 },
-  ];
+
   const currentId = parseInt(req.params.id);
   const concreteUser = users.find((user) => user.id === currentId); // currently am not using typescript here
   if (!concreteUser) {
-    res
+    return res
       .status(404)
       .json({ message: `cannot find user with the id: ${currentId}` });
   }
-  res.json(concreteUser); //We use not send but json for returning object and arrays
+  return res.json(concreteUser); //We use not send but json for returning object and arrays
+});
+app.get("/about", (req, res) => {
+  const requestedAge = parseInt(req.query.age);
+  if (requestedAge) {
+    const filteredUser = users.filter((user) => user.age === requestedAge);
+    return res.json(filteredUser);
+  } else {
+    return res.json(users);
+  }
 });
 app.listen(port, () => {
-  console.log(`Server: http://localhost:${port}`);
+  console.log(`Server: http://localhost:${port}/about?age=22`);
   console.log(`About:  http://localhost:${port}/api/users/1`);
 });
 // in order to install nodemon you must know that nodemon is a dev dependency, it will not be used when project starts so we have to install --save-dev by simply writing -D

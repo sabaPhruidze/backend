bf2fa7f24a0f8887f30fad5ea0bd4f9bf9703b5a
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 15:29:07 +0400

    now we have get method using express and first server is made

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+//@ts-check
 //here we will import .dotenv in order to than use hidden Token, secret keys and port using process.env
 require("dotenv").config();
 // //let's install express in order to make the process of creating server easier
-// const express = require("express");
-// const app = express();
-const port = process.env.PORT;
-console.log(port);
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Send only returns string");
+});
+app.get("/about", (req, res) => {
+  const user = [
+    { id: 1, name: "Saba" },
+    { id: 2, name: "Tofu" },
+  ];
+  res.json(user); //We use not send but json for returning object and arrays
+});
+app.listen(port, () => {
+  console.log(`Server: http://localhost:${port}`);
+  console.log(`About:  http://localhost:${port}/about`);
+});

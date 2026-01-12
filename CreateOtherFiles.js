2c01538c39c6d77eb2e009faa3bded6ce7b50a2d
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 14:57:03 +0400

    now created additional file for remove and writed full code for removing and creating

--- CreateOtherFiles.js
+++ CreateOtherFiles.js
@@ -1,1 +1,17 @@
 //@ts-check
+const fs = require("fs").promises;
+const path = require("path");
+
+const createFile = async () => {
+  try {
+    const direction = path.join(__dirname, "server.js");
+    await fs.writeFile(direction, "//Hi file is created");
+    await fs.appendFile(direction, "//This text is added in the created file");
+    const readFile = fs.readFile(direction, "utf-8");
+    console.log(readFile);
+    await fs.unlink("folder/forRemove.js");
+  } catch (error) {
+    console.error(error);
+  }
+};
+createFile();

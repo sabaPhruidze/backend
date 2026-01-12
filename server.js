9112cfe78f411f81dbb5f02506af3caa1ee11980
Saba Phruidze <sabaphruidze01@gmail.com>
2026-01-12 17:23:55 +0400

    MVC part without Model finished , by doing SOC in reality , it became seperated and less code is written on each file

--- server.js
+++ server.js
@@ -6,35 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // middleware
-app.get("/", (req, res) => {
-  res.send("Send only returns string");
-});
-app.get("/api/users/:id", (req, res) => {
-  // let's change this and make dynamic
 
-  const currentId = parseInt(req.params.id);
-  const concreteUser = users.find((user) => user.id === currentId); // currently am not using typescript here
-  if (!concreteUser) {
-    return res
-      .status(404)
-      .json({ message: `cannot find user with the id: ${currentId}` });
-  }
-  return res.json(concreteUser); //We use not send but json for returning object and arrays
-});
-app.get("/about", (req, res) => {
-  const requestedAge = parseInt(req.query.age);
-  if (requestedAge) {
-    const filteredUser = users.filter((user) => user.age === requestedAge);
-    return res.json(filteredUser);
-  } else {
-    return res.json(users);
-  }
-});
-app.get("/api/users");
-// in order to check Our CRUD does it function or not we I use vscode extenshion thunder client, now I will start the post method
-app.post("/api/users");
-app.put("/api/users/:id");
-app.delete("/api/users/:id");
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api/users", userRoutes);
+
 app.listen(port, () => {
   console.log(`Server: http://localhost:${port}/about?age=22`);
   console.log(`About:  http://localhost:${port}/api/users`);

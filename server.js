6ba445b233c419538b11952e1a311d1868ad5bd5
Saba <sabaphruidze01@gmail.com>
2026-02-06 18:26:22 +0400

    now learned what to do if I have uncaughtException or unhandleRejection

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+//testing how the uncaughtExpection and unhandled rejection works
+process.on("uncaughtException", (err) => {
+  console.error("critical error, system must be shut down", err.message);
+  process.exit(1);
+});
+process.on("unhandledRejection", (err) => {
+  console.error("unhandled promise rejection", err.message);
+  process.exit(1);
+});
 //here we will import .dotenv in order to than use hidden Token, secret keys and port using process.env
 require("dotenv").config();
 // //let's install express in order to make the process of creating server easier
@@ -14,6 +23,8 @@ const userRoutes = require("./routes/userRoutes");
 
 app.use("/api/users", userRoutes);
 app.use("/api/fs", fsRoutes);
+nonExistentFunction();
+
 app.listen(port, () => {
   console.log(`Server: http://localhost:${port}/about?age=22`);
   console.log(`About:  http://localhost:${port}/api/users`);

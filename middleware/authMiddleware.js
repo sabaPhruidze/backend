b0dceb01595ac047668b26af2e88d82d93368627
Saba <sabaphruidze01@gmail.com>
2026-02-12 17:32:16 +0400

    succesfully added middleware which controls token

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModels");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization?.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+      return next();
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({ message: "not authorized" });
+    }
+  }
+
+  return res.status(401).json({ message: "There is no token" });
+};
+
+module.exports = { protect };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/userController");
-
+const { protect } = require("../middleware/authMiddleware");
 router.get("/", userController.getUsers);
-router.get("/login", userController.loginUsers);
+router.post("/login", protect, userController.loginUsers);
 router.post("/register", userController.registerUsers);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);

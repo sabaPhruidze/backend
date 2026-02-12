75d8ffcf66c39c8c1c8cd9ab6f1d0e36ce536d09
Saba <sabaphruidze01@gmail.com>
2026-02-12 16:59:12 +0400

    I through I had here middleware which should know how to compare token to our sign but since it is not First I will add that and than move on learning zod

--- controller/userController.js
+++ controller/userController.js
@@ -24,7 +24,6 @@ const registerUsers = async (req, res) => {
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
     const user = await User.create({ name, email, password: hashedPassword });
 
     res.status(201).json({
@@ -86,6 +85,7 @@ const generateToken = (id) => {
     expiresIn: "30d",
   });
 };
+
 module.exports = {
   getUsers,
   registerUsers,

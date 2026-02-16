7c48e5a7f29b697056af5b508a73f57fecd4ed82
Saba <sabaphruidze01@gmail.com>
2026-02-16 10:25:29 +0400

    feat(useSchema): reWrote zod schema for register only

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
+//@ts-check
 const express = require("express");
 const router = express.Router();
+
 const userController = require("../controller/userController");
 const { protect } = require("../middleware/authMiddleware");
+
 router.get("/", userController.getUsers);
 router.post("/login", protect, userController.loginUsers);
-router.post("/register", userController.registerUsers);
+router.post("/register", protect, userController.registerUsers);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
---
+++ validation/useSchemas.js
@@ -0,0 +1,19 @@
+const { z } = require("zod");
+
+// Receiving only name,email,password and strict() will not allow other fields such as (role , isAdmin...)
+const registerSchema = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(2, "Name must be at least 2 characters")
+    .max(30, "Name must be at most 30 characters"),
+  email: z
+    .email("Invalid Email format")
+    .trim()
+    .max(50, "Email must be at most 50 characters"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(8, "Password must be at least 8 characters")
+    .max(30, "Password must be at most 30 characters"),
+});
+module.exports = { registerSchema };

b5de8a8139860ae73e86dd3fe5b459e0d0ba6f3f
Saba <sabaphruidze01@gmail.com>
2026-02-17 17:34:45 +0400

    update('userSchema') added to more one query and one for dynamic id

--- validation/userSchema.js
+++ validation/userSchema.js
@@ -1,24 +1,44 @@
 const { z } = require("zod");
 
 // Receiving only name,email,password and strict() will not allow other fields such as (role , isAdmin...)
-const registerSchema = z.object({
-  name: z
-    .string({ required_error: "Name is required" })
-    .trim()
-    .min(2, "Name must be at least 2 characters")
-    .max(30, "Name must be at most 30 characters"),
-  email: z
-    .email("Invalid Email format")
-    .trim()
-    .max(50, "Email must be at most 50 characters"),
-  password: z
-    .string({ required_error: "Password is required" })
-    .min(8, "Password must be at least 8 characters")
-    .max(30, "Password must be at most 30 characters"),
+const registerSchema = z
+  .object({
+    name: z
+      .string({ required_error: "Name is required" })
+      .trim()
+      .min(2, "Name must be at least 2 characters")
+      .max(30, "Name must be at most 30 characters"),
+    email: z
+      .email("Invalid Email format")
+      .trim()
+      .max(50, "Email must be at most 50 characters"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(8, "Password must be at least 8 characters")
+      .max(30, "Password must be at most 30 characters"),
+  })
+  .strict();
+const loginSchema = z
+  .object({
+    email: z.email("Invalid Email"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+  })
+  .strict();
+const userIdParamSchema = z.object({
+  id: z.string().min(1, "id is required"),
 });
-const loginSchema = z.object({
-  email: z.email("Invalid Email"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-});
+const userQuerySchema = z
+  .object({
+    search: z.string().trim().min(1).optional(),
+    role: z.enum(["user", "admin"]).optional(),
+    page: z.coerce.number().int().min(1).optional(), // z.coerce.number() string to number
+    limit: z.coerce.number().int().min(1).max(100).optional(), //int() X-2.5 O-2
+  })
+  .strict(); // if there be a parameter that is not within schema it will not allow
 
-module.exports = { loginSchema, registerSchema };
+module.exports = {
+  loginSchema,
+  registerSchema,
+  userIdParamSchema,
+  userQuerySchema,
+};

5c2c04b4288486b758c96e24d205cdf9915ed463
Saba <sabaphruidze01@gmail.com>
2026-02-19 18:30:07 +0400

    create(server.ts) moved everything from it to server.js and npm run build and uncaughtException and unhandledRejection had issues in types so corrected this way. now everything works, also corrected tsconfig and package json as needed

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,13 @@
 //testing how the uncaughtExpection and unhandled rejection works
-process.on("uncaughtException", (err: unknown) => {
-  const message = err instanceof Error ? err.message : String(err);
-  console.error("critical error, system must be shut down", message);
-  process.exit(1);
+process.on("uncaughtException", (err) => {
+    const message = err instanceof Error ? err.message : String(err);
+    console.error("critical error, system must be shut down", message);
+    process.exit(1);
 });
-process.on("unhandledRejection", (err: unknown) => {
-  const message = err instanceof Error ? err.message : String(err);
-  console.error("unhandled promise rejection", message);
-  process.exit(1);
+process.on("unhandledRejection", (err) => {
+    const message = err instanceof Error ? err.message : String(err);
+    console.error("unhandled promise rejection", message);
+    process.exit(1);
 });
 //here we will import .dotenv in order to than use hidden Token, secret keys and port using process.env
 require("dotenv").config();
@@ -18,17 +18,14 @@ const connectDB = require("./config/db");
 const app = express();
 const port = process.env.PORT || 3000;
 const fsRoutes = require("./routes/fsRoutes");
-
 connectDB();
 app.use(express.json()); // middleware
-
 const userRoutes = require("./routes/userRoutes");
-
 app.use("/api/users", userRoutes);
 app.use("/api/fs", fsRoutes);
 assert(process.env.JWT_SECRET, "JWT_SECRET IS IMPORTANT");
 app.listen(port, () => {
-  console.log(`Server: http://localhost:${port}/about?age=22`);
-  console.log(`About:  http://localhost:${port}/api/users`);
+    console.log(`Server: http://localhost:${port}/about?age=22`);
+    console.log(`About:  http://localhost:${port}/api/users`);
 });
 // in order to install nodemon you must know that nodemon is a dev dependency, it will not be used when project starts so we have to install --save-dev by simply writing -D

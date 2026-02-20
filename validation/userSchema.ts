951e863e80a75b8226de0a13dc43a2e9a1cb5ba2
Saba <sabaphruidze01@gmail.com>
2026-02-20 10:42:55 +0400

    update(useSchema):turned it into typescript and even through it is typescript it uses import and export like ESM eventhroguh it is commonjs besides I have learned by typescript help that V4 version of zod do not use required_error and invalid_type_error so I removed them

--- validation/userSchema.ts
+++ validation/userSchema.ts
@@ -1,10 +1,10 @@
-const { z } = require("zod");
+import { z } from "zod";
 
 // Receiving only name,email,password and strict() will not allow other fields such as (role , isAdmin...)
-const registerSchema = z
+export const registerSchema = z
   .object({
     name: z
-      .string({ required_error: "Name is required" })
+      .string("Name is required")
       .trim()
       .min(2, "Name must be at least 2 characters")
       .max(30, "Name must be at most 30 characters"),
@@ -13,41 +13,40 @@ const registerSchema = z
       .trim()
       .max(50, "Email must be at most 50 characters"),
     password: z
-      .string({ required_error: "Password is required" })
+      .string("Password is required")
       .min(8, "Password must be at least 8 characters")
       .max(30, "Password must be at most 30 characters"),
   })
   .strict();
-const loginSchema = z
+export const loginSchema = z
   .object({
     email: z.email("Invalid Email"),
     password: z.string().min(8, "Password must be at least 8 characters"),
   })
   .strict();
-const userIdParamSchema = z.object({
+export const userIdParamSchema = z.object({
   id: z.string().min(1, "id is required"),
 });
-const userQuerySchema = z
+export const userQuerySchema = z
   .object({
     search: z.string().trim().min(1).optional(),
     role: z.enum(["user", "admin"]).optional(),
     page: z.coerce
-      .number({ invalid_type_error: "write number only" })
+      .number()
+      .refine((v) => Number.isFinite(v), { message: "write number only" })
       .int("it must be 1,2,3... not 2.5 , 344.24234234")
       .min(1)
       .optional(), // z.coerce.number() string to number
     limit: z.coerce
-      .number({ invalid_type_error: "write number only" }) // for not allowing other than number
+      .number() // for not allowing other than number (since it has to be number we give it with refine because z.coerce.number() turn in nubmer)
+      .refine((v) => Number.isFinite(v), { message: "write number only" })
       .int("it must be 1,2,3... not 2.5 , 344.24234234")
       .min(1)
       .max(100)
       .optional(), //int() X-2.5 O-2
   })
   .strict(); // if there be a parameter that is not within schema it will not allow
-
-module.exports = {
-  loginSchema,
-  registerSchema,
-  userIdParamSchema,
-  userQuerySchema,
-};
+export type RegisterBody = z.infer<typeof registerSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>;
+export type USerIdParams = z.infer<typeof userIdParamSchema>;
+export type UserQuery = z.infer<typeof userQuerySchema>;

2496d275655a1aecc60ff8701c1b20b0410082d1
Saba <sabaphruidze01@gmail.com>
2026-02-23 11:15:35 +0400

    feat(userModels): updated typescript part but also added select:false in order not to write every time -password and when necessary than I will write +password to retrieve that as well

---
+++ models/userModels.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, model, type InferSchemaType } from "mongoose";
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "It is necessary to write your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is necessary"],
+      unique: true, //there has to be no identical emails
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is necesarry"],
+      select: false, // By this until I write +password it will not be received
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export type UserDoc = InferSchemaType<typeof userSchema>; // like in zod here inferSchemaType creates type from userchema
+const User = mongoose.models.User || model<UserDoc>("User", userSchema); //for solving this error OverwriteModelError: Cannot overwrite 'User' model once compiled.
+export default User;

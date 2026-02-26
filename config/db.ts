41ac226d273e401054fb9b1fd100abbc67366a10
Saba <sabaphruidze01@gmail.com>
2026-02-26 18:23:02 +0400

    feat(db): added user.createindexes in order to create the indexes written in usermodels immediatelly when server runs

--- config/db.ts
+++ config/db.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import User from "../models/userModels";
 const connectDB = async (): Promise<void> => {
   try {
     const mongoUri = process.env.MONGO_URI;
@@ -6,6 +7,7 @@ const connectDB = async (): Promise<void> => {
       throw new Error("MONGO_URI is missing in environment variables");
     }
     const conn = await mongoose.connect(process.env.MONGO_URI || "");
+    await User.createIndexes();
     console.log(`connected succesfully ${conn.connection.host}`);
   } catch (error) {
     const message = error instanceof Error ? error.message : String(error);

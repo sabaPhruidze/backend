c2d201d6e0c6ba892cea66e2a2d998f865f7b444
Saba <sabaphruidze01@gmail.com>
2026-05-03 17:40:40 +0400

    feat(userRoutes): updated delete, put and get methods where dynamic id is the route

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { restrictTo } from "../middleware/roleMiddleware";
+import { restrictTo, allowSelfOrAdmin } from "../middleware/roleMiddleware";
 import userController from "../controller/userController";
 import {
   loginSchema,
@@ -10,6 +10,7 @@ import {
 
 import { protect } from "../middleware/authMiddleware";
 import { validate } from "../middleware/validate";
+
 const router = Router();
 
 router.get(
@@ -28,8 +29,9 @@ router.get(
 );
 router.get(
   "/:id",
+  protect, //access token check
   validate(userIdParamSchema, "params", "Params Validation Errors"),
-  protect,
+  allowSelfOrAdmin, // only check's its acccount
   userController.getUserById,
 );
 router.post(
@@ -44,7 +46,19 @@ router.post(
 );
 router.post("/logout", protect, userController.logout);
 router.post("/refresh", userController.refreshAccessToken);
-router.put("/:id", protect, userController.updateUser);
-router.delete("/:id", protect, userController.deleteUser);
+router.put(
+  "/:id",
+  protect,
+  validate(userIdParamSchema, "params", "Params Validation Errors"),
+  allowSelfOrAdmin,
+  userController.updateUser,
+);
+router.delete(
+  "/:id",
+  protect,
+  validate(userIdParamSchema, "params", "Params validation Error"),
+  allowSelfOrAdmin,
+  userController.deleteUser,
+);
 
 export default router;

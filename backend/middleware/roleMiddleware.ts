0964bf3263dfe9fca729e84fa8c51daae44e0145
Saba <sabaphruidze01@gmail.com>
2026-05-03 17:33:01 +0400

    feat(roleMiddleware): added allowselforRole function which allowes everything to admin and restricts user only working on it's individual '

--- backend/middleware/roleMiddleware.ts
+++ backend/middleware/roleMiddleware.ts
@@ -17,3 +17,31 @@ export const restrictTo =
     }
     return next();
   };
+
+export const allowSelfOrAdmin = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!req.user) {
+    return res.status(401).json({
+      statu: "fail",
+      message: "Not authorized",
+    });
+  }
+  //   admin is allowed to do whatever he want's on user
+  if (req.user.name === "admin") {
+    return next();
+  }
+  // from route we receive the user id on which it act's
+  const requestedUserId = req.params.id;
+  // the id of an user which is logged in through token came turning into string
+  const loggedInUserId = req.user._id.toString();
+  if (loggedInUserId === requestedUserId) {
+    return next();
+  }
+  return res.status(403).json({
+    status: "fail",
+    message: "You can only access your own account",
+  });
+};
